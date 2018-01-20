767d3cb2eb1753d8986c503d5fe7ffcc11b2f78c
Omkar Langhe <langhe.omkar96@gmail.com>
2018-01-20 20:14:37 +0530

    Add files via upload

--- week2/addition.c
+++ week2/addition.c
@@ -4,7 +4,8 @@ int main(){
  int f,s,r;
  printf("Enter first number:\n");
  scanf("%d%d",&f,&s);
- printf("Result: %d\n",f+s);
+ printf("Result:\tADD:%d\n\tSUB:%d\n\tMULTIPLY:%d\n\tDIVISION:%d\n\tMODULUS:%d\n",f+s,f-s,f*s,f/s,f%s);
+ 
  
  return 0;  
 }
