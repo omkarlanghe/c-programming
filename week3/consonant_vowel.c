b655363fde3f41209e7049718431f4c580bd226c
Omkar Langhe <langhe.omkar96@gmail.com>
2018-01-22 18:47:08 +0530

    program using switch case

---
+++ week3/consonant_vowel.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main(){
+ char input;
+ printf("Enter any character:\n");
+ scanf("%c",&input);
+ switch(input)
+ {
+ 	case 'a':
+ 	printf("VOWEL\n");
+ 	break;
+ 	
+ 	case 'e':
+ 	printf("VOWEL\n");
+ 	break;
+ 	
+ 	case 'i':
+ 	printf("VOWEL\n");
+ 	break;
+ 	
+ 	case 'o':
+ 	printf("VOWEL\n");
+ 	break;
+ 	
+ 	case 'u':
+ 	printf("VOWEL\n");
+ 	break;
+ 	
+ 	default:
+ 	printf("CONSONANT\n");
+ 	break;
+ 	
+ }
+ return 0;
+ 
+}
